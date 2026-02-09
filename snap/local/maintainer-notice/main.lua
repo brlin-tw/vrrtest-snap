0cbafc0541bc02f28cf4017d0e3c9ba0f0859b3d
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2026-02-10 02:14:22 +0800

    fix(maintainer-notice): Fix typos in message
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- snap/local/maintainer-notice/main.lua
+++ snap/local/maintainer-notice/main.lua
@@ -1,7 +1,7 @@
 -- Snap maintainer notice to provide important info regarding the snap
--- Copyright 2024 林博仁(Buo-ren, Lin) <buo.ren.Lin@gmail.com>
+-- Copyright 2026 林博仁(Buo-ren Lin) <buo.ren.Lin@gmail.com>
 -- SPDX-License-Identifier: CC-BY-SA-4.0
-local message = "Hello, this is Buo-ren, Lin, the VRRTest snap's maintainer.\n\nI would like to inform you that this is an unofficial distribution of the VRRTest application.  Please report all issues regarding the usage of this snap to the snap's own issue tracker.\n\n    Issues · Buo-ren, Lin / Unofficial snap packaging for VRRTest · GitLab\n    https://gitlab.com/brlin/vrrtest-snap/-/issues\n\nThank you for you cooperation!\n\nPress any key to continue:"
+local message = "Hello, this is Buo-ren Lin, the VRRTest snap's maintainer.\n\nI would like to inform you that this is an unofficial distribution of the VRRTest application.  Please report all issues regarding the usage of this snap to the snap's own issue tracker.\n\n    Issues · Buo-ren Lin / Unofficial snap packaging for VRRTest · GitLab\n    https://gitlab.com/brlin/vrrtest-snap/-/issues\n\nThank you for your cooperation!\n\nPress any key to continue:"
 local x = 100
 local y = 100
 local backgroundColor = {1, 1, 1, 1}
